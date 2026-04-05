6b9caebf54d2287c22da61ae8d980a57309e5863
Martin Paz <159047775+martinpf03@users.noreply.github.com>
2026-04-05 20:21:35 +0200

    Update TrustProxies.php

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -7,13 +7,12 @@ use Illuminate\Http\Middleware\TrustProxies as Middleware;
 
 class TrustProxies extends Middleware
 {
-    /**
-     * Trust all proxies (necesario para Render)
-     */
     protected $proxies = '*';
 
-    /**
-     * Headers correctos para detectar HTTPS detrás de proxy
-     */
-    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+    protected $headers =
+        Request::HEADER_X_FORWARDED_FOR |
+        Request::HEADER_X_FORWARDED_HOST |
+        Request::HEADER_X_FORWARDED_PORT |
+        Request::HEADER_X_FORWARDED_PROTO |
+        Request::HEADER_X_FORWARDED_AWS_ELB;
 }
