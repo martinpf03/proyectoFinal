80bf3d106d98ee94e9b9511ce891892389f4bf0a
Martin Paz <159047775+martinpf03@users.noreply.github.com>
2026-04-05 20:16:41 +0200

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,10 +1,19 @@
 <?php
-namespace App\Http\Middleware; 
-use Illuminate\Http\Middleware\TrustProxies as Middleware;
-use Illuminate\Http\Request; class TrustProxies extends Middleware { 
-    /** * The trusted proxies for this application. * * @var array<int, string>|string|null */ 
-    protected $proxies = "*"; 
-    /** * The headers that should be used to detect proxies. * * @var int */ 
-    protected $headers = Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_HOST | Request::HEADER_X_FORWARDED_PORT | Request::HEADER_X_FORWARDED_PROTO | Request::HEADER_X_FORWARDED_AWS_ELB; 
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+
+class AppServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        //
+    }
+
+    public function boot(): void
+    {
+        URL::forceScheme('https');
+    }
 }
-?>
