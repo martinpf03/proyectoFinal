d2b9850ff1c4a0531774e14808611976c65e35f4
Martin Paz <159047775+martinpf03@users.noreply.github.com>
2026-04-06 15:25:40 +0200

    Update server.php

--- server.php
+++ server.php
@@ -1,13 +1,15 @@
 <?php
 
+$publicPath = __DIR__ . '/public';
+
 $uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) ?? ''
 );
 
-// Si el archivo existe → servirlo directamente
-if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
+// 🔥 SERVIR ARCHIVOS ESTÁTICOS (VITE)
+if ($uri !== '/' && file_exists($publicPath . $uri)) {
     return false;
 }
 
-// Si no → Laravel
-require_once __DIR__.'/public/index.php';
+// 🔥 SI NO → LARAVEL
+require_once $publicPath . '/index.php';
